3b9696ed09b1ba4d01031d97e59b7cc721ea39ee
casval2023 <132903453+casval2023@users.noreply.github.com>
2023-11-10 21:10:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,28 @@ import subprocess
 
 # タイトルの設定
 st.title("Settings Configuration")
+
+# 設定ファイルのパス
+config_file_path = 'settings.json'
+
+# 設定入力用のフォームを作成
+with st.form("settings_form"):
+    st.write("Please enter the configuration settings:")
+    ngrok_authtoken = st.text_input("NGROK_AUTHTOKEN")
+    line_access_token = st.text_input("LINE_ACCESS_TOKEN")
+    line_channel_secret = st.text_input("LINE_CHANNEL_SECRET")
+    submitted = st.form_submit_button("Submit")
+
+    # フォームが送信された場合、入力された設定をJSONファイルとして保存
+    if submitted:
+        settings = {
+            "NGROK_AUTHTOKEN": ngrok_authtoken,
+            "LINE_ACCESS_TOKEN": line_access_token,
+            "LINE_CHANNEL_SECRET": line_channel_secret
+        }
+        with open(config_file_path, 'w') as config_file:
+            json.dump(settings, config_file, indent=4)
+
+        st.success("Settings saved successfully!")
+        # `main.py`をサブプロセスとして起動
+        subprocess.run(["python", "main.py"])
